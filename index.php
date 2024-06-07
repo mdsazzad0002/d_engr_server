47dcd26ca59ff7a86308a0f93c8ad67aa793910e
MD. SAZZAD <mdsazzad0002@gmail.com>
2024-06-07 13:59:58 +0600

    data

--- index.php
+++ index.php
@@ -7,3 +7,5 @@ if (!isset($_SESSION['welcome'])) {
     require_once 'home-1/index.php';
     require_once 'assets/custom/visitor.php';
 }
+
+error_reporting(E_ALL); ini_set('display_errors', 1); ?>
