dd6d21a300567dff965d0d20436e05ad443cf5d1
MD. SAZZAD <mdsazzad0002@gmail.com>
2024-06-21 19:39:20 +0600

    redirect_disable

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-if (!isset($_SESSION['welcome'])) {
-    $_SESSION['welcome'] = 'welcome';
-    echo '<script>window.location.href="demo"</script>';
-} else {
+// if (!isset($_SESSION['welcome'])) {
+//     $_SESSION['welcome'] = 'welcome';
+//     echo '<script>window.location.href="demo"</script>';
+// } else {
     require_once 'home-1/index.php';
     require_once 'assets/custom/visitor.php';
-}
+// }
 
