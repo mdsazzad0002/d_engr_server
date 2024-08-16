b9b85ffba286af1243f27085d2efb640a37500b2
MD. SAZZAD <mdsazzad0002@gmail.com>
2024-08-16 13:11:46 +0600

    feature code

--- home-2/code.php
+++ home-2/code.php
@@ -6,7 +6,7 @@
             <br>
 
             <div class="d-flex gap-3 flex-wrap align-items-center justify-content-center">
-                <a class="btn btn-secondary" href="#">Learn About Feature</a>
+                <a class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#staticBackdrop" href="#">Learn About Feature</a>
                 <a class="btn" target="_blank" href="https://code.dengrweb.com">See Live</a>
             </div>
         </div>
@@ -20,3 +20,53 @@
         padding: 140px 0;
     }
 </style>
+<!-- Button trigger modal -->
+<!-- Button trigger modal -->
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Features of D Codecamp</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <ul>
+          
+            <li>  Guest
+                <ul>
+                    <li> Create Post as Anonymous</li>
+                    <li> Create Comment as Anonymous</li>
+                    <li> Create Vote as Anonymous</li>
+                </ul>
+            </li>
+            <li>  Users
+                <ul>
+                    <li> Full Control Post</li>
+                    <li> Full Control Comment</li>
+                    <li> Full Control Vote</li>
+                    <li> Full Control Social link</li>
+                    <li> Full Control Category Favorite</li>
+                    <li>Feed show by category wise</li>
+
+                </ul>
+            </li>
+            <li>Common
+                <ul>
+                    <li>Markdown Support</li>
+                    <li>User Contribution graph</li>
+                    <li>User Contribution post vote comment manage</li>
+                    <li>Easy way to create post and comment</li>
+                    <li>Visitor detection daily</li>
+                </ul>
+            </li>
+           
+        </ul>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>
